08fd7612af3325c6b6e0af980d1013aaa5ab4ed9
thomas-bc <thomas.boyerchammard@gmail.com>
2020-06-02 11:56:11 -0700

    Paris clustering of matches

--- req_analysis/__init__.py
+++ req_analysis/__init__.py
@@ -1,3 +1,1 @@
-from req_analysis.model import Model
 from req_analysis.requirement import Requirement
-from req_analysis.context_cluster import ContextCluster
--- req_analysis/requirement.py
+++ req_analysis/requirement.py
@@ -1,7 +1,12 @@
 import spacy
-
-from req_analysis.libs.neo4j_wrapper import get_weighted_neighbors
+import networkx as nx
 from req_analysis.libs.metrics import fuzzy_match_score
+from req_analysis.libs.neptune_wrapper import node_distance
+
+from paris import paris
+from paris.utils import select_clustering
+
+import time
 
 nlp_np = spacy.load("en_core_web_sm")
 merge_nps = nlp_np.create_pipe("merge_noun_chunks")
@@ -12,21 +17,22 @@ nlp_np.add_pipe(merge_nps)
 class Requirement():
 
 
-    def __init__(self, req_id, req_text):
-        self.uuid = req_id
+    def __init__(self, uri, req_text):
+        self.text_uri = uri
         self.text = req_text
         self.tokens = []
         self.transclusion_relations = []
 
         for token in nlp_np(self.text):
-            self.tokens.append(dict(text=token.text, pos=token.pos_))
+            self.tokens.append(dict(text=token.text, pos=token.pos_, token_id=token.i))
 
+        self.req_subgraph = None
 
 
     def match_req_tokens(self, model_elements, match_threshold, pos_list=["NOUN", "PROPN"]):
         '''Takes in a Requirement object and optional configuration
-        Returns a list of matches between the tokens in the requirement and the rest of the model
-        Will match on the 'name' attribute of the model elements'''
+        Returns a list of matches between the tokens in the requirement and the list of model_elements
+        Will match on the 'name' attribute of the model_elements dictionnaries'''
 
         # In all req tokens
         for token in self.tokens:
@@ -37,16 +43,87 @@ class Requirement():
 
                 for element in model_elements:
                     
-                    fuzzy_score = fuzzy_match_score(token['text'],  element)
+                    fuzzy_score = fuzzy_match_score(token['text'],  element['name'])
 
                     if fuzzy_score < match_threshold:
 
-                        if found_match == None:
-                            found_match = dict(token=token['text'], model_element=element, score=fuzzy_score)
-                        elif fuzzy_score < found_match['score']:
-                            found_match = dict(token=token['text'], model_element=element, score=fuzzy_score)
-
-                if found_match != None:
-                    self.transclusion_relations.append(found_match)
+                        found_match = dict(token=token, model_element=element, score=fuzzy_score)
+                        self.transclusion_relations.append(found_match)
 
         return self.transclusion_relations
+
+
+    def init_match_subgraph(self, g):
+        number_matches = len(self.transclusion_relations)
+        req_subgraph = nx.Graph()
+
+        # We want one node per matched token and not per model element matched
+        # The more it is referenced, the more important it is (if the text says 'APS' 10 times, APS has to be important)
+        for i in range(number_matches):
+            req_subgraph.add_node(i, **self.transclusion_relations[i])
+
+        for i in range(number_matches):
+            for j in range(i+1, number_matches):
+                el_i, el_j = self.transclusion_relations[i]['model_element'], self.transclusion_relations[j]['model_element']
+
+                if el_i == el_j:
+                    dist_ij = 0.1
+                    print(i, j)
+                    print('The 2 model elements are the same')
+                else:
+                    print(i, j)
+                    print(el_i, el_j)
+                    time1 = time.time()
+                    try:
+                        dist_ij = node_distance(g, el_i['uri'].replace('https://opencae.jpl.nasa.gov/mms/rdf/element/', ''), el_j['uri'].replace('https://opencae.jpl.nasa.gov/mms/rdf/element/', ''))
+                        print()
+                        print('SUCCESS in ', time.time()-time1, 's ', dist_ij)
+                    except:
+                        print('FAILURE in ', time.time()-time1, 's:  ')
+                        dist_ij = 11
+                    print('_________')
+
+                # We use 1/dist_ij because the Paris algorithm uses higher_weight=greater_proximity convention
+                req_subgraph.add_edge(i, j, weight=dist_ij)
+
+        self.req_subgraph = req_subgraph
+        return req_subgraph
+
+
+    def match_clustering(self):
+
+        paris_dendrogram = paris.paris(self.req_subgraph)
+        looper = True
+        k = 1
+        max_k = self.req_subgraph.number_of_nodes()
+
+        print(paris_dendrogram)
+        print(max_k)
+        while looper and k < max_k:
+
+            L = select_clustering(paris_dendrogram, k)
+            print(L, select_clustering(paris_dendrogram, k+1))
+            looper = self.check_continue(select_clustering(paris_dendrogram, k+1))
+            k += 1
+
+        return L
+
+
+    
+    def check_continue(self, L):
+        '''Takes in a list returned by select_clustering and checks that no cluster has 2 times the same token in it'''
+        
+        # For each cluster
+        for cluster in L:
+
+            # checks that no 2 nodes in the cluster has the same source token
+            for i in range(len(cluster)):
+                token_i = self.req_subgraph.nodes(data=True)[cluster[i]]['token']
+                for j in range(i+1, len(cluster)):
+
+                    token_j = self.req_subgraph.nodes(data=True)[cluster[j]]['token']
+
+                    if token_i['token_id']==token_j['token_id']:
+                        return False
+
+        return True
