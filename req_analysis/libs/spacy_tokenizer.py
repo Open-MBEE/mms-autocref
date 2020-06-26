5afc3cd41b8de46953b86f52cb3c00e8f0cd3c26
thomas-bc <thomas.boyerchammard@gmail.com>
2020-06-26 16:31:45 -0700

    Removed '<' and '>' from prefix and suffix

--- req_analysis/libs/spacy_tokenizer.py
+++ req_analysis/libs/spacy_tokenizer.py
@@ -5,20 +5,26 @@ from spacy.util import compile_prefix_regex, compile_infix_regex, compile_suffix
 from spacy import util
 from spacy.lang.tokenizer_exceptions import TOKEN_MATCH
 from spacy.tokens import Token
-# Token.set_extension('tag', default=False)
+
 
 
 def create_custom_tokenizer(nlp):
     
     infixes = tuple([r"\<[\w\/]*\>"]) +  nlp.Defaults.infixes
-    prefixes = tuple([r"\<[\w\/]*\>"]) +  nlp.Defaults.prefixes
-    suffixes = tuple([r"\<[\w\/]*\>"]) +  nlp.Defaults.suffixes
     infix_re = spacy.util.compile_infix_regex(infixes)
-    prefix_re = compile_prefix_regex(prefixes)
-    suffix_re = compile_suffix_regex(suffixes)
+    prefix_re = compile_prefix_regex()
+    suffix_re = compile_suffix_regex()
 
-    return Tokenizer(nlp.vocab, prefix_search=prefix_re.search,
-                                suffix_search=suffix_re.search,
+    prefixes = list(nlp.Defaults.prefixes)
+    prefixes.remove('<')
+    prefixes = tuple(prefixes)
+    suffixes = list(nlp.Defaults.suffixes)
+    suffixes.remove('>')
+    suffixes = tuple(suffixes)
+    prefix_search = (util.compile_prefix_regex(prefixes).search)
+    suffix_search = (util.compile_suffix_regex(suffixes).search)
+
+    return Tokenizer(nlp.vocab, prefix_search=prefix_search,
+                                suffix_search=suffix_search,
                                 infix_finditer=infix_re.finditer,
-                                token_match=nlp.tokenizer.token_match,
-                                rules=nlp.Defaults.tokenizer_exceptions)
+                                token_match=None)
